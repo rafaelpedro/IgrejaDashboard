2de2426fc91ee9959d730e91a64d633412853a4c
Rafael Alencar Pedro <rafael.apedro95@gmail.com>
2025-11-10 17:28:46 -0300

    atualiza .gitignore"

---
+++ IgrejaDashboard.Api/Models/ApiError.cs
@@ -0,0 +1,9 @@
+﻿namespace IgrejaDashboard.Api.Models
+{
+    public class ApiError
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string? Details { get; set; }
+    }
+}

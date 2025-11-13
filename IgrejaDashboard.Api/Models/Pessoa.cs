ad3f815d8269f23e52e8666520890a49edb5f6e9
Rafael Alencar Pedro <rafael.apedro95@gmail.com>
2025-11-13 09:48:53 -0300

    Adicionado validações no model Pessoa

--- IgrejaDashboard.Api/Models/Pessoa.cs
+++ IgrejaDashboard.Api/Models/Pessoa.cs
@@ -6,9 +6,13 @@ namespace IgrejaDashboard.Api.Models
     {
         [Key]
         public int Codigo { get; set; }
+        [Required]
         public string Nome { get; set; } = string.Empty;
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Sexo { get; set; }=string.Empty;
+        [Required]
         public SituacaoPessoa Status { get; set; } = SituacaoPessoa.Visitante;
     }
 }
